f2ebb033741210954356f11c450cec65f76f92e8
Felipe Nunes <febatera.soares@gmail.com>
2022-08-12 19:43:38 -0300

    (Refactor): Add port if .env not configured

--- src/server.js
+++ src/server.js
@@ -4,4 +4,4 @@ const logger = require("./utils/logger");
 const { name, version } = require("../package.json");
 const { PORT } = process.env;
 
-app.listen(PORT, () => logger.info(`${name} version ${version} running on port ${PORT}`));
+app.listen(PORT || 3555, () => logger.info(`${name} version ${version} running on port ${PORT|| 3555}`));
